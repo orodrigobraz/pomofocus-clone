b64e401cc91033c1b97652b4e9e614703c2869f7
Rodrigo Braz <orodrigobraz18@gmail.com>
2025-09-18 20:55:31 -0300

    Corrigir base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pomodoro/',
+  base: '/pomofocus-clone/',
   server: {
     port: 3000,
     open: true
